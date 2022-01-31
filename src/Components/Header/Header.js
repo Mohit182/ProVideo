18ef1d8c6e0a0e20fff1fdc270f9d37df6fe7ef7
Mohit Gupta <mohitguptatech18@gmail.com>
2022-01-31 17:47:54 +0530

    header component created

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,38 @@
 import React from "react";
 import "./Header.css";
+import logoheader from "../../Assets/logo-header.png";
+import {
+  Apps,
+  Menu,
+  Notifications,
+  Search,
+  VideoCall,
+} from "@material-ui/icons";
+import { Avatar, Button } from "@material-ui/core";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header-left">
+        <Menu className="header-menuicon" />
+        <img className="header-logo" src={logoheader} alt="logo" />
+      </div>
+
+      <div className="header-center">
+        <input type="text" className="header-input" placeholder="Search" />
+        <Button className="header-btn">
+          <Search className="header-searchIcon" />
+        </Button>
+      </div>
+
+      <div className="header-right">
+        <VideoCall />
+        <Apps />
+        <Notifications />
+        <Avatar />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
